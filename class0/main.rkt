9fd24303b3a7e144c57142272e1425d56f9cf40e
dvanhorn <dvanhorn@ccs.neu.edu>
2010-11-11 20:15:54 +0000

    Added require and provide to exports of class0.
    git-svn-id: http://svn.lambda-calcul.us/oo/trunk@39 5597a609-ba74-427a-a62e-28e6ec40ac72

--- class0/main.rkt
+++ class0/main.rkt
@@ -3,5 +3,6 @@
          lang/htdp-intermediate-lambda
          test-engine/racket-tests)
 (provide (all-from-out "define-class.rkt")
-         define test
-         (except-out (all-from-out lang/htdp-intermediate-lambda) define))
+         (except-out (all-from-out lang/htdp-intermediate-lambda) define require)
+         define test require provide
+         all-defined-out only-in all-from-out)
