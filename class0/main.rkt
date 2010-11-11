a99601ae60c0812d1046e1a02e93fe7ec46c858b
samth <samth@racket-lang.org>
2010-11-11 20:53:10 +0000

    Fix define/require
    git-svn-id: http://svn.lambda-calcul.us/oo/trunk@42 5597a609-ba74-427a-a62e-28e6ec40ac72

--- class0/main.rkt
+++ class0/main.rkt
@@ -1,8 +1,8 @@
 #lang racket/base
 (require "define-class.rkt"
-         lang/htdp-intermediate-lambda
+         (except-in lang/htdp-intermediate-lambda define require)
          test-engine/racket-tests)
 (provide (all-from-out "define-class.rkt")
-         (except-out (all-from-out lang/htdp-intermediate-lambda) define require)
+         (all-from-out lang/htdp-intermediate-lambda)
          define test require provide
          all-defined-out only-in all-from-out)
