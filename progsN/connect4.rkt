6769ef1baa024d7464322c78ed5b21bcfb7e3462
samth <samth@racket-lang.org>
2010-12-06 22:28:43 +0000

    much better, but still not perfect for diagonals
    git-svn-id: http://svn.lambda-calcul.us/oo/trunk@47 5597a609-ba74-427a-a62e-28e6ec40ac72

--- progsN/connect4.rkt
+++ progsN/connect4.rkt
@@ -1,5 +1,5 @@
 #lang racket
-(require 2htdp/image 2htdp/universe)
+(require 2htdp/image 2htdp/universe racket/pretty lang/posn)
 
 ;; A Player is one of 'red or 'black
 ;; A Cell is one of Player or #f
@@ -7,23 +7,29 @@
 ;; columns are represented bottom = first
 ;; A Board is (list Column Column Column Column Column Column Column)
 ;; boards are represented leftmost = first
-;; A Game is (game Player Board)
-(struct game (next board))
+;; A Game is (game Player Board Posn)
+(struct game (next board pos) #:transparent)
 
 (define COLS 7)
 (define ROWS 6)
 
+(define RADIUS 20)
+
+(define SIZE 400)
+
+(define left-margin (/ (- SIZE (* 2 RADIUS COLS)) 2))
+
 (define empty-col (make-list ROWS #f))
 (define empty-board (make-list COLS empty-col))
-(define initial-game (game 'red empty-board))
+(define initial-game (game 'red empty-board (make-posn 0 0)))
 
 ;; render : Board -> Image
 ;; create a picture representing the board state
 (define (render board)
   (define (render-cell cell)
-    (cond [(not cell) (circle 20 "outline" "black")]
-          [(symbol=? cell 'black) (circle 20 "solid" "black")]
-          [(symbol=? cell 'red) (circle 20 "solid" "red")]))
+    (cond [(not cell) (circle RADIUS "outline" "black")]
+          [(symbol=? cell 'black) (circle RADIUS "solid" "black")]
+          [(symbol=? cell 'red) (circle RADIUS "solid" "red")]))
   (define (render-col col)
     (apply above (reverse (map render-cell col))))
   (apply beside (map render-col board)))
@@ -64,7 +70,7 @@
 (define (play-turn g col)
   (define new-board (play (game-board g) col (game-next g)))
   (if new-board
-      (game (flip-player (game-next g)) new-board)
+      (game (flip-player (game-next g)) new-board (game-pos g))
       g))
 
 (define (winner-col? c)
@@ -79,26 +85,36 @@
 (define (transpose b)
   (apply map list b))
 
+(define (diagonalize b)
+  (for/list ([n (in-range COLS)])
+    (append
+     (for/list ([k (in-range n ROWS)])
+      (list-ref (list-ref b n) k))
+     (make-list n #f))))
 ;; winner? : Game -> Boolean
 ;; has someone won the game?
 (define (winner? g)
   (or (ormap winner-col? (game-board g))
-      (ormap winner-col? (transpose (game-board g)))))
+      (ormap winner-col? (transpose (game-board g)))
+      (ormap winner-col? (transpose (diagonalize (game-board g))))
+      (ormap winner-col? (transpose (diagonalize (map reverse (game-board g)))))))
 
 ;; Game -> Scene
 (define (render-game g)
+  (define next (cond [(symbol=? (game-next g) 'black) (circle RADIUS "solid" "black")]
+                     [(symbol=? (game-next g) 'red) (circle RADIUS "solid" "red")]))
   (place-image
    (render (game-board g))
-   200 150
+   (/ SIZE 2) 150
    (place-image
-    (cond [(symbol=? (game-next g) 'black) (circle 20 "solid" "black")]
-          [(symbol=? (game-next g) 'red) (circle 20 "solid" "red")])
-    200 350
-    (empty-scene 400 400))))
+    next
+    (posn-x (game-pos g)) (posn-y (game-pos g))
+    (empty-scene SIZE SIZE))))
 
 ;; Game KeyEvent -> Game
 ;; play in the column denoted by `k'
 (define (handle-key g k)
+  (when (equal? "p" k) (pretty-print g))
   (define n (string->number k))
   (if (and n (<= 1 n 7))
       (play-turn g (sub1 n))
@@ -113,9 +129,14 @@
           [(symbol=? w 'red) (text "Red Player Wins!" 36 "green")]))
   (overlay str s))
 
+(define (handle-mouse g x y e)
+  (cond [(mouse=? e "button-up")
+         (play-turn g (quotient (- x left-margin) (* 2 RADIUS)))]
+        [else (game (game-next g) (game-board g) (make-posn x y))]))
+
 (big-bang
  initial-game
  (to-draw render-game)
- (on-key handle-key)
+ #;(on-key handle-key)
+ (on-mouse handle-mouse)
  (stop-when winner? render-winner))
-
