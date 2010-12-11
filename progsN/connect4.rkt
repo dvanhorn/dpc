c6b1a8d08d62b1d39eea48a24e69e3bdd01f772c
samth <samth@racket-lang.org>
2010-12-11 20:42:27 +0000

    AI working
    git-svn-id: http://svn.lambda-calcul.us/oo/trunk@51 5597a609-ba74-427a-a62e-28e6ec40ac72

--- progsN/connect4.rkt
+++ progsN/connect4.rkt
@@ -8,8 +8,9 @@
 ;; columns are represented bottom = first
 ;; A Board is (list Column Column Column Column Column Column Column)
 ;; boards are represented leftmost = first
-;; A Game is (game Player Board Posn)
-(struct game (next board pos) #:transparent)
+;; A Game is (game Player Board Posn Wins)
+;; A Wins is (list Number Number) - red wins, black wins
+(struct game (next board pos wins) #:transparent)
 
 (define COLS 7)
 (define ROWS 6)
@@ -27,8 +28,8 @@
 
 (define empty-col (make-list ROWS #f))
 (define empty-board (make-list COLS empty-col))
-(define initial-game (game 'red empty-board (make-posn 0 0)))
-(define (make-new-game p) (game 'red empty-board p))
+(define initial-game (game 'red empty-board (make-posn 0 0) (list 0 0)))
+(define (make-new-game p w) (game 'red empty-board p w))
 
 ;; render : Board -> Image
 ;; create a picture representing the board state
@@ -77,7 +78,7 @@
 (define (play-turn g col)
   (define new-board (play (game-board g) col (game-next g)))
   (if new-board
-      (game (flip-player (game-next g)) new-board (game-pos g))
+      (game (flip-player (game-next g)) new-board (game-pos g) (game-wins g))
       g))
 
 
@@ -91,6 +92,56 @@
       [(not (first c)) (loop (rest c) 0 #f)]
       [(and color (symbol=? color (first c))) (loop (rest c) (add1 cnt) color)]
       [else (loop (rest c) 1 (first c))])))
+
+(define ai%
+  (class object%
+    (init-field learn?) (init-field random?)
+    (field (memory (make-hash)))
+    ;; Randomly chooses an index into `status' where `sym' can be found
+    (define (random-index-of status sym)
+      (choose (for/list ([(s i) (in-indexed status)] #:when (eq? s sym))
+                i)))
+    
+    ;; choose the best move in `g' with one-ply lookahead
+    (define (pick-move g)
+      (define status (evaluate g))
+      (or (random-index-of status 'win)
+          (random-index-of status 'unknown)
+          (random-index-of status 'lose)))
+    
+    ;; choose a random element of l, or #f if empty
+    ;; Listof[X] -> X or f
+    (define (choose l) (if (null? l) #f (list-ref l (if random? (random (length l)) 0))))
+    
+    (define (is-bad? e)
+      (and (not (member 'win e)) (not (member 'unknown e))))
+    
+    ;; produce an evaluation of the state of `g' for the next player
+    ;; Game -> Listof[One of 'full 'win 'lose 'unknown]
+    (define (evaluate g)
+      (define (new-eval)
+        (for/list ([i (in-range COLS)])
+          (cond [(full? (list-ref (game-board g) i)) 'full]
+                [(winner? (play-turn g i)) 'win]
+                [(has-winning-move? (play-turn g i)) 'lose]
+                [else 'unknown])))
+      (define eval (hash-ref! memory (game-board g) new-eval))
+      (when (and (is-bad? eval) learn?)
+        (hash-update! memory last-board
+                      (λ (last-eval)
+                        (list-set last-eval last-move 'lose))))
+      eval)
+    
+    (define last-move #f)
+    (define last-board empty-board)
+    
+    (define/public (play g)
+      (define m
+        (pick-move g))
+      (set! last-move m)
+      (set! last-board (game-board g))
+      m)
+    (super-new)))
 
 ;; Board -> Board^T
 (define (transpose b) (apply map list b))
@@ -139,7 +190,8 @@
                           (black red red #f #f #f)
                           (black red #f #f #f #f)
                           (red black black black #f #f))
-                        (make-posn 83 -7)))
+                        (make-posn 83 -7)
+                        (list 0 0)))
               #t)
 
 ;; Is this column full?
@@ -153,65 +205,85 @@
            #:when (winner? (play-turn g i)))
     #t))
 
-;; Randomly chooses an index into `status' where `sym' can be found
-(define (random-index-of status sym)
-  (choose (for/list ([(s i) (in-indexed status)] #:when (eq? s sym))
-            i)))
+(define (render-board g)
+  (place-image (render (game-board g))
+               (/ SIZE 2) 150
+               (empty-scene SIZE SIZE)))
 
-;; choose the best move in `g' with one-ply lookahead
-(define (pick-move g)
-  (define status (evaluate g))
-  (or (random-index-of status 'win)
-      (random-index-of status 'unknown)
-      (random-index-of status 'lose)))
-       
-;; choose a random element of l, or #f if empty
-;; Listof[X] -> X or f
-(define (choose l) (if (null? l) #f (list-ref l (random (length l)))))
-
-;; produce an evaluation of the state of `g' for the next player
-;; Game -> Listof[One of 'full 'win 'lose 'unknown]
-(define (evaluate g)
-  (for/list ([i (in-range COLS)])
-    (cond [(full? (list-ref (game-board g) i)) 'full]
-          [(winner? (play-turn g i)) 'win]
-          [(has-winning-move? (play-turn g i)) 'lose]
-          [else 'unknown])))
+(define (render-wins w)
+  (beside (text (number->string (first w)) 30 "red") (text (number->string (second w)) 30 "black")))
+  
 
 ;; Game -> Scene
 (define (render-game g)
   (define next (cond [(symbol=? (game-next g) 'black) (circle RADIUS "solid" "black")]
                      [(symbol=? (game-next g) 'red) (circle RADIUS "solid" "red")]))
-  (define board-scene (place-image (render (game-board g))
-                                   (/ SIZE 2) 150
-                                   (empty-scene SIZE SIZE)))
+  (if (game-over? g)
+      (render-winner g)
+      (place-image 
+       (render-wins (game-wins g))
+       (/ SIZE 2) 350
+       (place-image
+        next
+        (posn-x (game-pos g)) (posn-y (game-pos g))
+        (render-board g)))))
+
+(define (red-wins g) (first (game-wins g)))
+(define (black-wins g) (second (game-wins g)))  
+;; inc-winner : Game -> Wins
+(define (inc-winner g)
   (if (winner? g)
-      board-scene
-      (place-image
-       next
-       (posn-x (game-pos g)) (posn-y (game-pos g))
-       board-scene)))
+      (case (flip-player (game-next g))
+        [(red) (list (add1 (red-wins g)) (black-wins g))]
+        [(black) (list (red-wins g) (add1 (black-wins g)))])
+      (game-wins g)))
 
 ;; Game KeyEvent -> Game
 ;; "n" -> new game
 ;; "p" -> print game state
+;; "h" -> pick a move for me
 (define (handle-key g k)
-  (cond [(key=? "n" k) (make-new-game (game-pos g))]
+  (cond [(key=? "n" k) (make-new-game (game-pos g) (inc-winner g))]
         [(key=? "p" k) (displayln g) g]
+        [(key=? "a" k) (if (game-over? g) (auto-play (make-new-game (game-pos g) (inc-winner g))) (auto-play g))]
+        [(and (not (game-over? g)) (key=? "h" k)) (human-play g (send assistant play g))]
         [else g]))
   
 (define (all-full? g) (andmap full? (game-board g)))
 
+(define (game-over? g) (or (winner? g) (all-full? g)))
+
 ;; Game -> Scene
 (define (render-winner g)
   (define w (flip-player (game-next g)))
-  (define s (render-game g))
+  (define s (render-board g))
   (define str
     (cond 
       [(all-full? g) (text "The Game is a Tie." 36 "green")]
       [(symbol=? w 'black) (text "Black Player Wins!" 36 "green")]
       [(symbol=? w 'red) (text "Red Player Wins!" 36 "green")]))
-  (overlay str s))
+  (place-image 
+   (render-wins (inc-winner g))
+   (/ SIZE 2) 350
+   (overlay str s)))
+
+(define (auto-play g)
+  (if (game-over? g)
+      g
+      (auto-play
+       (if (symbol=? (game-next g) 'black)
+           (play-turn g (send computer-player play g))
+           (play-turn g (send assistant play g))))))
+
+(define computer-player (new ai% [learn? #t] [random? #f]))
+
+(define assistant (new ai% [learn? #t] [random? #t]))
+
+(define (human-play g col) 
+  (define played (play-turn g col))
+  (if (game-over? played)
+      played
+      (play-turn played (send computer-player play played))))
 
 ;; Handle a mouse event in game `g'
 ;; If the button is released, play in that column.
@@ -221,17 +293,14 @@
          (cond 
            [(or (winner? g) (all-full? g)) g]
            [(and (> x left-margin) (<= 0 col (sub1 COLS)))
-            (define played (play-turn g col))
-            (if (winner? played)
-                played
-                (play-turn played (pick-move played)))]
+            (human-play g col)]
            [else g])]
-         [else (game (game-next g) (game-board g) (make-posn x y))]))
+         [else (game (game-next g) (game-board g) (make-posn x y) (game-wins g))]))
 
 (define (go)
   (big-bang
    initial-game
-   (to-draw (λ (g) (if (or (winner? g) (all-full? g)) (render-winner g) (render-game g))))
+   (to-draw render-game)
    (on-key handle-key)
    (on-mouse handle-mouse)
    #;(stop-when (λ (g) (or (winner? g) (all-full? g))) render-winner)))
