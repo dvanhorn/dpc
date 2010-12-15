55c1b3df3d046900c9c39c8196ad96536bfa97e3
dvanhorn <dvanhorn@ccs.neu.edu>
2010-12-15 22:34:16 +0000

    Initial complex impl.
    git-svn-id: http://svn.lambda-calcul.us/oo/trunk@65 5597a609-ba74-427a-a62e-28e6ec40ac72

---
+++ progs0/complex.rkt
@@ -0,0 +1,95 @@
+#lang class0
+
+(define-class complex%
+  (fields real imag)
+  
+  (define/public (=? n)
+    (and (= (field real)
+            (send n real))
+         (= (field imag)
+            (send n imag))))
+    
+  (define/public (plus n)
+    (new complex% 
+         [real (+ (field real) (send n real))]
+         [imag (+ (field imag) (send n imag))]))
+    
+  (define/public (minus n)
+    (new complex% 
+         [real (- (field real) (send n real))]
+         [imag (- (field imag) (send n imag))]))
+    
+  (define/public (times n)
+    (new complex%
+         [real (- (* (field real) (send n real))
+                  (* (field imag) (send n imag)))]
+         [imag (+ (* (field imag) (send n real))
+                  (* (field real) (send n imag)))]))
+  
+  (define/public (div n)
+    (new complex%
+         [real (/ (+ (* (field real) (send n real))
+                     (* (field imag) (send n imag)))
+                  (+ (sqr (send n real))
+                     (sqr (send n imag))))]
+         [imag (/ (- (* (field imag) (send n real))
+                     (* (field real) (send n imag)))
+                  (+ (sqr (send n real))
+                     (sqr (send n imag))))]))
+    
+  (define/public (sq)
+    (times this))
+  
+  ;; -> Number
+  (define/public (mag)
+    (sqrt (+ (sqr (field real))
+             (sqr (field imag)))))
+  
+  (define/public (sqroot)
+    (new complex% 
+         [real (sqrt (/ (+ (mag) (field real)) 2))]
+         [imag (* (sqrt (/ (- (mag) (field real)) 2))
+                  (if (negative? (field imag))
+                      -1
+                      +1))]))
+  
+  (define/public (to-number)
+    (make-rectangular (field real)
+                      (field imag))))
+
+
+
+; Some example Complex values.
+(define c-1  (new complex% [real -1] [imag 0]))
+(define c0+0 (new complex% [real 0] [imag 0]))
+(define c2+3 (new complex% [real 2] [imag 3]))
+(define c3+5 (new complex% [real 4] [imag 5]))
+; Verify the imaginary unit property.
+(check-expect (send c-1 mag) 1)
+(check-expect (send c-1 sqroot) 
+              (new complex% 
+                   [real 0]
+                   [imag 1]))
+(check-expect (send (send (send c-1 sqroot) sq) =? c-1)
+              true)
+(check-expect (send (send (new complex% [real 0] [imag 1]) sq) =? c-1)
+              true)
+; Arithmetic on complex numbers.
+(check-expect (send c0+0 =? c0+0)
+              true)
+(check-expect (send c0+0 =? c2+3)
+              false)
+(check-expect (send (send c2+3 plus c3+5) =?
+                    (new complex% [real 6] [imag 8]))
+              true)
+(check-expect (send (send c2+3 minus c3+5) =?
+                    (new complex% [real -2] [imag -2]))
+              true)
+(check-expect (send (send c2+3 times c3+5) =?
+                    (new complex% [real -7] [imag 22]))
+              true)
+(check-expect (send (send c2+3 div c3+5) =?
+                    (new complex% [real 23/41] [imag 2/41]))
+              true)
+(check-expect (send (new complex% [real 3] [imag 4]) mag)              
+              5)
