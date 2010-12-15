45dead23ef722d526fe68f4686a3558faada74f4
dvanhorn <dvanhorn@ccs.neu.edu>
2010-12-15 23:08:56 +0000

    Add random testing to complex program.
    git-svn-id: http://svn.lambda-calcul.us/oo/trunk@66 5597a609-ba74-427a-a62e-28e6ec40ac72

--- progs0/complex.rkt
+++ progs0/complex.rkt
@@ -1,4 +1,5 @@
 #lang class0
+(require (only-in racket for in-range unless))
 
 (define-class complex%
   (fields real imag)
@@ -57,8 +58,6 @@
     (make-rectangular (field real)
                       (field imag))))
 
-
-
 ; Some example Complex values.
 (define c-1  (new complex% [real -1] [imag 0]))
 (define c0+0 (new complex% [real 0] [imag 0]))
@@ -93,3 +92,36 @@
               true)
 (check-expect (send (new complex% [real 3] [imag 4]) mag)              
               5)
+
+
+
+(define (random-complex)
+  (new complex% 
+       [real (- (random 100) 50)]
+       [imag (- (random 100) 50)]))
+
+(define epsilon 0.00001)
+
+#;
+(for ([i (in-range 50000)])
+  (let ((r (random-complex)))
+    (unless (=~ (send (send r sqroot) to-number)
+                (sqrt (send r to-number))
+                epsilon)
+      (error "BARF" i r))))
+
+(for ([i (in-range 50000)])
+  (let ((n (random-complex))
+        (d (random-complex)))    
+    (unless (zero? (send d to-number))
+      (unless (= (send (send n div d) to-number)
+                 (/ (send n to-number)
+                    (send d to-number)))
+        (error "HURL" i n d)))))
+
+
+
+(check-within 3+4.4i 3 1)
+;(=~ 3+4.4i 3 1)
+
+
