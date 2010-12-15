30a637c37463f0992471516244328f795276feeb
dvanhorn <dvanhorn@ccs.neu.edu>
2010-12-15 05:42:56 +0000

    Ported (what there was of) tetris to class0.
    git-svn-id: http://svn.lambda-calcul.us/oo/trunk@59 5597a609-ba74-427a-a62e-28e6ec40ac72

--- progs0/tetris.rkt
+++ progs0/tetris.rkt
@@ -1,12 +1,10 @@
-#lang racket
+#lang class0
 ;; Tetris in Object-Oriented Style
 ;; Copyright (c) 2010 David Van Horn
 ;; Licensed under the Academic Free License version 3.0
 
-(require racket/class)
 (require 2htdp/universe)
 (require 2htdp/image)
-(require test-engine/racket-tests)
 
 (define block-size   20)  ;; in Pixels
 (define board-width  10)  ;; in Blocks
@@ -16,38 +14,31 @@
 ;; Data Definitions
 
 ;; A Block is a (new block% [x Nat] [y Nat] [color Color])
-(define block% 
-  (class* object% ()
-    (super-new)
-    (init-field [(the-x x)]
-                [(the-y y)]
-                [(the-color color)])
-    (define/public (x) the-x)
-    (define/public (y) the-y)
-    (define/public (color) the-color)
+(define-class block% 
+  (fields x y color)
     
-    ;; Nat Nat -> Block
-    (define/public (move dx dy)
-      (new this%
-           [x (+ the-x dx)]
-           [y (+ the-y dy)]
-           [color the-color]))
-    
-    ;; Block -> Boolean
-    (define/public (=? b)
-      (and (= the-x (send b x))
-           (= the-y (send b y))))
-    
-    ;; Posn -> Block
-    (define/public (rotate-ccw p)
-      (new this% 
-           [x (+ (send p x)
-                 (- (send p y)
-                    the-y))]
-           [y (+ (send p y)
-                 (- the-x
-                    (send p x)))]
-           [color the-color]))
+  ;; Nat Nat -> Block
+  (define/public (move dx dy)
+    (new block%
+         [x (+ (field x) dx)]
+         [y (+ (field y) dy)]
+         [color (field color)]))
+  
+  ;; Block -> Boolean
+  (define/public (=? b)
+    (and (= (field x) (send b x))
+         (= (field y) (send b y))))
+  
+  ;; Posn -> Block
+  (define/public (rotate-ccw p)
+    (new block% 
+         [x (+ (send p x)
+               (- (send p y)
+                  (field y)))]
+         [y (+ (send p y)
+               (- (field x)
+                  (send p x)))]
+         [color (field color)]))
     
     ;; Posn -> Block
     (define/public (rotate-cw p)
@@ -55,14 +46,14 @@
     
     ;; -> Image
     (define/public (->image)
-      (rectangle (add1 block-size) (add1 block-size) 'solid the-color))
+      (rectangle (add1 block-size) (add1 block-size) 'solid (field color)))
     
     ;; Scene -> Scene
     (define/public (+scene scn)
       (place-image (->image)
-                   (+ (* the-x block-size) (/ block-size 2))
-                   (+ (* the-y block-size) (/ block-size 2))
-                   scn))))
+                   (+ (* (field x) block-size) (/ block-size 2))
+                   (+ (* (field y) block-size) (/ block-size 2))
+                   scn)))
 
 
 (check-expect (send (new block% [x 0] [y 0] [color 'black]) =?
@@ -91,232 +82,201 @@
 
 ;; It is useful for representing sets of values.
 
-(define set-interface
-  (interface () 
-    empty?    ;; -> Boolean
-    contains? ;; X -> Boolean
-    remove    ;; X -> [Set X]
-    union     ;; [Set X] -> [Set X]
-    intersect ;; [Set X] -> [Set X]
-    =?        ;; [Set X] -> Boolean
-    subset?   ;; [Set X] -> Bolean
-    map       ;; [X -> Y] [Y Y -> Boolean] -> [Set Y]
-    length    ;; -> Nat
-    fold      ;; [X Y -> Y] Y -> Y
-    filter))  ;; [X -> Boolean] -> [Set X]
+(define-interface set-interface
+  (empty?    ;; -> Boolean
+   contains? ;; X -> Boolean
+   remove    ;; X -> [Set X]
+   union     ;; [Set X] -> [Set X]
+   intersect ;; [Set X] -> [Set X]
+   =?        ;; [Set X] -> Boolean
+   subset?   ;; [Set X] -> Bolean
+   map       ;; [X -> Y] [Y Y -> Boolean] -> [Set Y]
+   length    ;; -> Nat
+   fold      ;; [X Y -> Y] Y -> Y
+   filter))  ;; [X -> Boolean] -> [Set X]
 
 ;; A [Set X] is one of:
-;; - (new mt-set% [= [X X -> Boolean]])
-;; - (new cons-set% [elem X] [rest [Set X]]),
-;;   where elem is *not* an element in rest.
-      
-(define set% 
-  (class* object% ()
-    (super-new)
-    (init-field [(the-= =)])
+;; - (new set% [= [X X -> Boolean]] [elems [Listof X]])
+;; where elems contains no duplicates.
+(define-class set% 
+  (fields = elems)
  
-    ;; -> [X X -> Boolean]
-    (define/public (=) the-=)
-    
-    ;; [Set X] -> Boolean
-    (define/public (=? s)
-      (and (send this subset? s)
-           (send s subset? this)))
-    
-    ;; [Set X] -> [Set X]
-    (define/public (union s)
-      (send this fold (λ (y s) (send s add y)) s))
-    
-    ;; -> Nat
-    (define/public (length)
-      (send this fold (λ (_ n) (add1 n)) 0))
-    
-    ;; [X -> Boolean] -> [Set X]
-    (define/public (filter f)
-      (send this fold
-            (λ (x s) (if (f x)
-                         (send s add x)
-                         s))
-            (new mt-set% [= the-=])))
-    
-    ;; [X -> Y] [Y Y -> Boolean] -> [Set Y]
-    (define/public (map f =)
-      (send this fold 
-            (λ (x s) (send s add (f x)))
-            (new mt-set% [= =])))))
-
-(define mt-set%
-  (class* set% (set-interface)
-    (super-new)
-    (define/public (empty?) true)
-    (define/public (contains? x) false)
-    (define/public (remove x)
-      this)
-    (define/public (add x)
-      (new cons-set% [elem x] [rest this]))
-    (define/public (intersect s)
-      this)
-    (define/public (subset? s)
-      true)
-    (define/public (fold f b)
-      b)))
-      
-
-(define cons-set%
-  (class* set% (set-interface)
-    (init-field [(the-elem elem)] [(the-rest rest)])
-    (super-new [= (send the-rest =)])
-    (inherit =)
-    
-    ;; -> Boolean
-    (define/public (empty?) false)
-    
-    ;; X -> Boolean
-    (define/public (contains? x)
-      (or ((=) the-elem x)
-          (send the-rest contains? x)))
-    
-    ;; X -> [Set X]
-    (define/public (remove x)
-      (if ((=) x the-elem)
-          the-rest
-          (new this% 
-               [elem the-elem]
-               [rest (send the-rest remove x)])))
-    
-    ;; [Set X] -> [Set X]
-    (define/public (intersect s)
-      (if (send s contains? the-elem)
-          (new this%
-               [elem the-elem]
-               [rest (send the-rest intersect s)])
-          (send the-rest intersect s)))
-    
-    ;; [Set X] -> Boolean
-    (define/public (subset? s)
-      (and (send s contains? the-elem)
-           (send the-rest subset? s)))
-    
-    ;; X -> [Set X]
-    (define/public (add x)
-      (cond [(contains? x) this]
-            [else
-             (new this% [elem x] [rest this])]))
-    
-    ;; [X Y -> Y] Y -> Y
-    (define/public (fold f b)
-      (f the-elem (send the-rest fold f b)))))
-   
-;; [X X -> Bolean] X ... -> [Set X]
+  ;; [Set X] -> Boolean
+  (define/public (=? s)
+    (and (send this subset? s)
+         (send s subset? this)))
+  
+  ;; [Set X] -> [Set X]
+  (define/public (union s)
+    (send this fold (λ (y s) (send s add y)) s))
+  
+  ;; -> Nat
+  (define/public (length)
+    (send this fold (λ (_ n) (add1 n)) 0))
+  
+  ;; [X -> Boolean] -> [Set X]
+  (define/public (filter f)
+    (send this fold
+          (λ (x s) (if (f x)
+                       (send s add x)
+                       s))
+          (new set% 
+               [= (field =)]
+               [elems empty])))
+  
+  ;; [X -> Y] [Y Y -> Boolean] -> [Set Y]
+  (define/public (map f =)
+    (send this fold 
+          (λ (x s) (send s add (f x)))
+          (new set% 
+               [= =]
+               [elems empty])))
+  
+  (define/public (empty?)
+    (not (cons? (field elems))))
+  
+  (define/public (contains? x)
+    (ormap (λ (y) ((field =) x y))
+           (field elems)))
+  
+  (define/public (remove x)
+    (new set%
+         [= (field =)]
+         [elems
+          (foldl (λ (y xs) 
+                   (cond [((field =) x y)
+                          xs]
+                         [else
+                          (cons y xs)]))
+                 empty
+                 (field elems))]))
+  
+  (define/public (add x)
+    (cond [(ormap (λ (y) ((field =) x y))
+                  (field elems))
+           this]
+          [else
+           (new set% 
+                [= (field =)]
+                [elems (cons x (field elems))])]))
+  
+  (define/public (intersect s)
+    (new set% 
+         [= (field =)]
+         [elems
+          (foldl (λ (x xs) 
+                   (cond [(ormap (λ (y) ((field =) x y))
+                                 (send s elems))
+                          (cons x xs)]
+                         [else xs]))
+                 empty
+                 (field elems))]))
+  
+  (define/public (subset? s)
+    (andmap (λ (x) (send s contains? x))
+            (field elems)))
+  
+  (define/public (fold f b)
+    (foldl f b (field elems))))
+  
+  
+;; [X X -> Bolean] [Listof X] -> [Set X]
 ;; Convenient constructor for sets.
-(define (set = . xs)
-  (cond [(empty? xs) (new mt-set% [= =])]
-        [else (send (apply set = (rest xs)) add (first xs))]))
-                
+(define (set = xs)
+  (new set% [= =] [elems xs]))
 
+(check-expect (send (set = empty) empty?) true)
+(check-expect (send (set = empty) contains? 5) false)
+(check-expect (send (send (set = empty) remove 5) empty?) true)
+(check-expect (send (send (set = empty) union (set = empty)) empty?) true)
+(check-expect (send (send (set = empty) intersect (set = empty)) empty?) true)
 
-(check-expect (send (set =) empty?) true)
-(check-expect (send (set =) contains? 5) false)
-(check-expect (send (send (set =) remove 5) empty?) true)
-(check-expect (send (send (set =) union (set =)) empty?) true)
-(check-expect (send (send (set =) intersect (set =)) empty?) true)
+(check-expect (send (set = (list 1)) =? (set = (list 1))) true)
+(check-expect (send (set = (list 1)) empty?) false)
+(check-expect (send (set = (list 1)) contains? 5) false)
+(check-expect (send (set = (list 1 5 2)) contains? 5) true)
+(check-expect (send (send (set = (list 1)) remove 5) empty?) false)
+(check-expect (send (send (set = (list 1)) union (set = empty)) empty?) false)
+(check-expect (send (send (set = (list 1)) intersect (set = empty)) empty?) true)
 
-(check-expect (send (set = 1) =? (set = 1)) true)
-(check-expect (send (set = 1) empty?) false)
-(check-expect (send (set = 1) contains? 5) false)
-(check-expect (send (set = 1 5 2) contains? 5) true)
-(check-expect (send (send (set = 1) remove 5) empty?) false)
-(check-expect (send (send (set = 1) union (set =)) empty?) false)
-(check-expect (send (send (set = 1) intersect (set =)) empty?) true)
+(check-expect (send (set = (list 1 1 1)) =? (set = (list 1))) true)
+(check-expect (send (set = (list 1)) =? (set = (list 1 1 1))) true)
+(check-expect (send (set = (list 1 2 3)) =? (set = (list 3 2 1))) true)
 
-(check-expect (send (set = 1 1 1) =? (set = 1)) true)
-(check-expect (send (set = 1) =? (set = 1 1 1)) true)
-(check-expect (send (set = 1 2 3) =? (set = 3 2 1)) true)
-
-(check-expect (send (set = 1 2 3 4) fold + 0) 10)
+(check-expect (send (set = (list 1 2 3 4)) fold + 0) 10)
 
 
 ;; Nat Nat -> Boolean
 (define (=%5 x y) (= (modulo x 5) (modulo y 5)))
-(check-expect (send (set =%5 1 2 3) =? (set =%5 6 7 8)) true)
+(check-expect (send (set =%5 (list 1 2 3)) =? (set =%5 (list 6 7 8))) true)
 
 ;; [Set X] [Set X] -> Boolean
 (define (s= s1 s2)
   (send s1 =? s2))
 
 ;; Comparing [Set [Set Nat]]
-(check-expect (send (set s= (set = 1 2 3) (set = 3 2 1)) =?
-                    (set s= (set = 1 2 3)))
+(check-expect (send (set s= (list (set = (list 1 2 3)) (set = (list 3 2 1)))) =?
+                    (set s= (list (set = (list 1 2 3)))))
               true)
               
-(check-expect (send (send (set = 1 2 3 6 7 8) map add1 =) contains? 1) false)
-(check-expect (send (send (set = 1 2 3 6 7 8) map add1 =) contains? 2) true)
-(check-expect (send (send (set = 1 2 3 6 7 8) map add1 =%5) length) 3)
+(check-expect (send (send (set = (list 1 2 3 6 7 8)) map add1 =) contains? 1) false)
+(check-expect (send (send (set = (list 1 2 3 6 7 8)) map add1 =) contains? 2) true)
+(check-expect (send (send (set = (list 1 2 3 6 7 8)) map add1 =%5) length) 3)
               
 
 ;; -----------------------------------------------------------------------
 ;; POSN
 
-(define posn%
-  (class* object% ()
-    (super-new)
-    (init-field [(the-x x)] [(the-y y)])
-    (define/public (x) the-x) ;; -> Nat
-    (define/public (y) the-y) ;; -> Nat
-    ;; Posn -> Boolean
-    (define/public (=? p)
-      (and (= the-x (send p x))
-           (= the-y (send p y))))
-    ;; Nat Nat -> Posn
-    (define/public (move dx dy)
-      (new this% [x (+ the-x dx)] [y (+ the-y dy)]))))
+(define-class posn%
+  (fields x y)
+  ;; Posn -> Boolean
+  (define/public (=? p)
+    (and (= (field x) (send p x))
+         (= (field y) (send p y))))
+  ;; Nat Nat -> Posn
+  (define/public (move dx dy)
+    (new posn% [x (+ (field x) dx)] [y (+ (field y) dy)])))
 
 
 ;; -----------------------------------------------------------------------
 (define EMPTY-BOARD
   (empty-scene (add1 (* board-width block-size)) 
                (add1 (* board-height block-size))))
-                         
+          
 ;; A Tetra is a (new tetra% [center Posn] [blocks [Set Block]])
 ;; The center point is the point around which the tetra rotates
 ;; when it is rotated.
-(define tetra%
-  (class* object% ()
-    (super-new)
-    (init-field [(the-center center)]
-                [(the-blocks blocks)])
-    (define/public (center) the-center)
-    (define/public (blocks) the-blocks)
-    (define/public (move dx dy)
-      (new this% 
-           [center (send the-center move dx dy)]
-           [blocks (send the-blocks map 
-                         (λ (b) (send b move dx dy))
-                         (λ (b1 b2) (send b1 =? b2)))]))
+(define-class tetra%
+  (fields center blocks)
+  
+  (define/public (move dx dy)
+    (new tetra% 
+         [center (send (field center) move dx dy)]
+         [blocks (send (field blocks) map 
+                       (λ (b) (send b move dx dy))
+                       (λ (b1 b2) (send b1 =? b2)))]))
+  
+  (define/public (rotate-ccw)
+    (new tetra% 
+         [center (field center)]
+         [blocks (send (field blocks) map
+                       (λ (b) (send b rotate-ccw (field center)))
+                       (λ (b1 b2) (send b1 =? b2)))]))
+  
+  (define/public (rotate-cw)
+    (new tetra% 
+         [center (field center)]
+         [blocks (send (field blocks) map
+                       (λ (b) (send b rotate-cw (field center)))
+                       (λ (b1 b2) (send b1 =? b2)))]))
+  
+  (define/public (+scene scene)
+    (send (field blocks) fold 
+          (λ (b scn) (send b +scene scn))
+          EMPTY-BOARD))
     
-    (define/public (rotate-ccw)
-      (new tetra% 
-           [center the-center]
-           [blocks (send the-blocks map
-                         (λ (b) (send b rotate-ccw the-center))
-                         (λ (b1 b2) (send b1 =? b2)))]))
-    
-    (define/public (rotate-cw)
-      (new tetra% 
-           [center the-center]
-           [blocks (send the-blocks map
-                         (λ (b) (send b rotate-cw the-center))
-                         (λ (b1 b2) (send b1 =? b2)))]))
-    
-    (define/public (+scene scene)
-      (send the-blocks fold 
-            (λ (b scn) (send b +scene scn))
-            EMPTY-BOARD))
-            
-    
-    (define/public (overlaps? bs)
-      (not (send (send bs intersect the-blocks) empty?)))
-))
+  (define/public (overlaps? bs)
+    (not (send (send bs intersect (field blocks)) empty?))))
 
 (define (block=? b1 b2) (send b1 =? b2))
 
@@ -324,10 +284,10 @@
   (send (new tetra% 
              [center (new posn% [x xc] [y yc])]
              [blocks (set block=?
-                          (new block% [x x1] [y y1] [color color])
-                          (new block% [x x2] [y y2] [color color])
-                          (new block% [x x3] [y y3] [color color])
-                          (new block% [x x4] [y y4] [color color]))])
+                          (list (new block% [x x1] [y y1] [color color])
+                                (new block% [x x2] [y y2] [color color])
+                                (new block% [x x3] [y y3] [color color])
+                                (new block% [x x4] [y y4] [color color])))])
         move 3 0))
         
 
@@ -340,14 +300,12 @@
    (build-tetra-blocks 'cyan   	1   -1      0 -1 1 -1 2 -1 0 -2)
    (build-tetra-blocks 'orange  1   -1      0 -1 1 -1 2 -1 1 -2)
    (build-tetra-blocks 'red     1   -1      0 -1 1 -1 1 -2 2 -2)
-   (build-tetra-blocks 'pink    1   -1      0 -2 1 -2 1 -1 2 -1)
-   ))
+   (build-tetra-blocks 'pink    1   -1      0 -2 1 -2 1 -1 2 -1)))
 
 ;; list-pick-random : [Listof a] -> a
 ;; Randomly pick an element from the list.
 (define (list-pick-random ls)
   (list-ref ls (random (length ls))))
-
 
 (define (full-row? bs i)
   (= board-width
@@ -365,55 +323,49 @@
     (elim-row board-height 0)))
 
 
-
 ;; A World is a (new world% [tetra Tetra] [blocks [Set Block]])
-(define world%
-  (class* object% ()
-    (super-new)
-    (init-field [(the-tetra tetra)]
-                [(the-blocks blocks)])
-    (define/public (tetra) the-tetra)
-    (define/public (blocks) the-blocks)
-    ;; -> World
-    (define/public (touchdown)
-      (new this% 
-           [tetra (list-pick-random tetras)]
-           [blocks (send (send (send the-tetra blocks) union the-blocks)
-                         eliminate-full-rows)]))
-    ;; Key -> World
-    (define/public (handle-key k)
-      (cond [(key=? k "left")
-             (move -1 0)]
-            [(key=? k "right")
-             (move 1 0)]
-            [(key=? k "down")
-             (jumpdown)]
-            [(key=? k "a")
-             (rotate-ccw)]
-            [(key=? k "s")
-             (rotate-cw)]
-            [else this]))
+(define-class world%
+  (fields tetra blocks)
+  
+  ;; -> World
+  (define/public (touchdown)
+    (new world% 
+         [tetra (list-pick-random tetras)]
+         [blocks (send (send (send (field tetra) blocks) union 
+                             (field blocks))
+                       eliminate-full-rows)]))
+  ;; Key -> World
+  (define/public (handle-key k)
+    (cond [(key=? k "left")
+           (move -1 0)]
+          [(key=? k "right")
+           (move 1 0)]
+          [(key=? k "down")
+           (jumpdown)]
+          [(key=? k "a")
+           (rotate-ccw)]
+          [(key=? k "s")
+           (rotate-cw)]
+          [else this]))
     
     ;; -> World
     (define/public (jumpdown) '...)
     (define/public (rotate-ccw)
       (new world% 
-           [tetra (send the-tetra rotate-ccw)]
-           [blocks the-blocks]))
+           [tetra (send (field tetra) rotate-ccw)]
+           [blocks (field blocks)]))
     (define/public (rotate-cw)
       (new world% 
-           [tetra (send the-tetra rotate-cw)]
-           [blocks the-blocks]))
+           [tetra (send (field tetra) rotate-cw)]
+           [blocks (field blocks)]))
     (define/public (move dx dy)
-      (try-new-tetra (send the-tetra move dx dy)))
+      (try-new-tetra (send (field tetra) move dx dy)))
     (define/public (try-new-tetra t)
       (cond [(or (<  (blocks-min-x (send t blocks)) 0)
                  (>= (blocks-max-x (send t blocks)) board-width)
-                 (send t overlaps? the-blocks))
+                 (send t overlaps? (field blocks)))
              this]
-	[else (new this% [tetra% t] [blocks the-blocks])]))
-    
-    ))
+	[else (new world% [tetra% t] [blocks (field blocks)])])))
     
 (define (blocks-min-x bs)
   (send bs fold (λ (b n) (min (send b x) n)) board-width))
@@ -421,6 +373,3 @@
   (send bs fold (λ (b n) (max (send b x) n)) 0))
 
                   
-
-(test)
-
