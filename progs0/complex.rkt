85ebbd0927f4a35cf00fe27c0d73283d84104e26
dvanhorn <dvanhorn@ccs.neu.edu>
2010-12-17 21:49:39 +0000

    complex ported to positional args.
    git-svn-id: http://svn.lambda-calcul.us/oo/trunk@75 5597a609-ba74-427a-a62e-28e6ec40ac72

--- progs0/complex.rkt
+++ progs0/complex.rkt
@@ -9,35 +9,35 @@
             (send n real))
          (= (field imag)
             (send n imag))))
-    
+  
   (define/public (plus n)
     (new complex% 
-         [real (+ (field real) (send n real))]
-         [imag (+ (field imag) (send n imag))]))
-    
+         (+ (field real) (send n real))
+         (+ (field imag) (send n imag))))
+  
   (define/public (minus n)
     (new complex% 
-         [real (- (field real) (send n real))]
-         [imag (- (field imag) (send n imag))]))
-    
+         (- (field real) (send n real))
+         (- (field imag) (send n imag))))
+  
   (define/public (times n)
     (new complex%
-         [real (- (* (field real) (send n real))
-                  (* (field imag) (send n imag)))]
-         [imag (+ (* (field imag) (send n real))
-                  (* (field real) (send n imag)))]))
+         (- (* (field real) (send n real))
+            (* (field imag) (send n imag)))
+         (+ (* (field imag) (send n real))
+            (* (field real) (send n imag)))))
   
   (define/public (div n)
     (new complex%
-         [real (/ (+ (* (field real) (send n real))
-                     (* (field imag) (send n imag)))
-                  (+ (sqr (send n real))
-                     (sqr (send n imag))))]
-         [imag (/ (- (* (field imag) (send n real))
-                     (* (field real) (send n imag)))
-                  (+ (sqr (send n real))
-                     (sqr (send n imag))))]))
-    
+         (/ (+ (* (field real) (send n real))
+               (* (field imag) (send n imag)))
+            (+ (sqr (send n real))
+               (sqr (send n imag))))
+         (/ (- (* (field imag) (send n real))
+               (* (field real) (send n imag)))
+            (+ (sqr (send n real))
+               (sqr (send n imag))))))
+  
   (define/public (sq)
     (times this))
   
@@ -48,30 +48,28 @@
   
   (define/public (sqroot)
     (new complex% 
-         [real (sqrt (/ (+ (mag) (field real)) 2))]
-         [imag (* (sqrt (/ (- (mag) (field real)) 2))
+         (sqrt (/ (+ (mag) (field real)) 2))
+         (* (sqrt (/ (- (mag) (field real)) 2))
                   (if (negative? (field imag))
                       -1
-                      +1))]))
+                      +1))))
   
   (define/public (to-number)
     (make-rectangular (field real)
                       (field imag))))
 
 ; Some example Complex values.
-(define c-1  (new complex% [real -1] [imag 0]))
-(define c0+0 (new complex% [real 0] [imag 0]))
-(define c2+3 (new complex% [real 2] [imag 3]))
-(define c3+5 (new complex% [real 4] [imag 5]))
+(define c-1  (new complex% -1 0))
+(define c0+0 (new complex% 0 0))
+(define c2+3 (new complex% 2 3))
+(define c3+5 (new complex% 4 5))
 ; Verify the imaginary unit property.
 (check-expect (send c-1 mag) 1)
 (check-expect (send c-1 sqroot) 
-              (new complex% 
-                   [real 0]
-                   [imag 1]))
+              (new complex% 0 1))
 (check-expect (send (send (send c-1 sqroot) sq) =? c-1)
               true)
-(check-expect (send (send (new complex% [real 0] [imag 1]) sq) =? c-1)
+(check-expect (send (send (new complex% 0 1) sq) =? c-1)
               true)
 ; Arithmetic on complex numbers.
 (check-expect (send c0+0 =? c0+0)
@@ -79,26 +77,26 @@
 (check-expect (send c0+0 =? c2+3)
               false)
 (check-expect (send (send c2+3 plus c3+5) =?
-                    (new complex% [real 6] [imag 8]))
+                    (new complex% 6 8))
               true)
 (check-expect (send (send c2+3 minus c3+5) =?
-                    (new complex% [real -2] [imag -2]))
+                    (new complex% -2 -2))
               true)
 (check-expect (send (send c2+3 times c3+5) =?
-                    (new complex% [real -7] [imag 22]))
+                    (new complex% -7 22))
               true)
 (check-expect (send (send c2+3 div c3+5) =?
-                    (new complex% [real 23/41] [imag 2/41]))
+                    (new complex% 23/41 2/41))
               true)
-(check-expect (send (new complex% [real 3] [imag 4]) mag)              
+(check-expect (send (new complex% 3 4) mag)              
               5)
 
 
 
 (define (random-complex)
   (new complex% 
-       [real (- (random 100) 50)]
-       [imag (- (random 100) 50)]))
+       (- (random 100) 50)
+       (- (random 100) 50)))
 
 (define epsilon 0.00001)
 
@@ -111,6 +109,13 @@
       (error "BARF" i r))))
 
 (for ([i (in-range 50000)])
+  (let ((r (random-complex)))
+    (unless (< (magnitude (- (send (send r sqroot) to-number)
+                             (sqrt (send r to-number))))
+               epsilon)
+      (error "BARF" i r))))
+
+(for ([i (in-range 50000)])
   (let ((n (random-complex))
         (d (random-complex)))    
     (unless (zero? (send d to-number))
@@ -119,9 +124,5 @@
                     (send d to-number)))
         (error "HURL" i n d)))))
 
-
-
-(check-within 3+4.4i 3 1)
-;(=~ 3+4.4i 3 1)
 
 
