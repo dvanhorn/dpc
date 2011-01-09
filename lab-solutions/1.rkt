d83fc2a81b22d3ea0274a7da03b47677af4d5beb
jdanbrown <jdanbrown@gmail.com>
2011-01-09 09:06:11 +0000

    Lab 1 in progress
    git-svn-id: http://svn.lambda-calcul.us/oo/trunk@100 5597a609-ba74-427a-a62e-28e6ec40ac72

---
+++ lab-solutions/1.rkt
@@ -0,0 +1,126 @@
+#lang racket
+
+; Questions:
+;  - big-bang macro or world objects?
+;  - style for calling methods on this?
+;  - #lang or ASL?
+
+; TODO:
+;  - named args -> positional args
+
+(require class0)
+(require 2htdp/image)
+(require 2htdp/universe)
+
+; A Ball is a (new ball% Number Number Number Color)
+(define-class ball%
+  (fields x y radius color)
+
+  ; -> Number
+  ; The diameter of the Ball
+  (define/public (diameter)
+    (* 2 pi (field radius)))
+
+  ; Number -> Ball
+  ; Scale the radius of the Ball by a factor of x
+  (define/public (scale-radius x)
+    (new ball% (field x) (field y) (* x (field radius)) (field color)))
+
+  ; -> Image
+  ; The image representing the Ball
+  (define/public (draw)
+    (circle (field radius) 'solid (field color)))
+
+  ; -> Ball
+  ; The next Ball in the animation sequence
+  (define/public (step)
+    (scale-radius 49/50)))
+
+; A Block is a (new block% Number Number Number Number Color)
+(define-class block%
+  (fields x y width height color)
+
+  ; -> Number
+  ; The length of the diagonal of the Block
+  (define/public (diagonal)
+    (sqrt (+ (sqr (field width)) (sqr (field height)))))
+
+  ; -> Image
+  ; The image representing the Block
+  (define/public (draw)
+    (rectangle (field width) (field height) 'solid (field color)))
+
+  ; -> Block
+  ; The next Block in the animation sequence
+  (define/public (step)
+    (new block%
+         (field x)
+         (+ 1 (field y))
+         (field width)
+         (field height)
+         (field color))))
+
+; random-between : Integer Integer -> Integer
+; Randomly choose a number between a and b (inclusive)
+(define (random-between a b)
+  (+ b (random (+ 1 (- b a)))))
+
+; choose : [Listof X] -> X
+; Randomly choose an element from the list
+(define (choose xs)
+  (list-ref xs (random (length xs))))
+
+; random-color : -> Color
+; Randomly choose a color from a set of common colors
+(define (random-color)
+  (choose (list "red" "blue" "green" "yellow" "orange" "purple" "black")))
+
+; spawn-ball : Number Number -> Ball
+; Create a Ball with random parameters at location (x,y)
+(define (spawn-ball x y)
+  (new ball% x y (random-between 10 20) (random-color)))
+
+; spawn-block : Number Number -> Block
+(define (spawn-block x y)
+  (new block% x y (random-between 10 20) (random-between 10 20) (random-color)))
+
+; A Creature is an object that understands the `x', `y', `draw', and `step'
+; messages
+
+; spawn-functions : [Listof (-> Creature)]
+(define spawn-functions
+  (list spawn-ball spawn-block))
+
+; creature-spawner : -> (Number Number -> Creature)
+(define (creature-spawner)
+  (choose (list spawn-ball spawn-block)))
+
+; A World is a [Listof Creature]
+
+; tick : World -> World
+; Advance the World
+(define (tick w)
+  (map (λ (c) (send c step)) w))
+
+; draw : World -> Image
+; Draw the World
+(define (draw w)
+  (foldr (λ (c scn) (place-image (send c draw)
+                                 (send c x)
+                                 (send c y)
+                                 scn))
+         (empty-scene 400 400)
+         w))
+
+; mouse : World Number Number MouseEvent -> World
+; Respond to mouse events
+(define (mouse w x y ev)
+  (cond
+    [(equal? ev "button-down")
+     (cons ((creature-spawner) x y) w)]
+    [else w]))
+
+(big-bang empty
+          (on-tick tick)
+          (on-draw draw)
+          (on-mouse mouse))
--- web/labs/1.scrbl
+++ web/labs/1.scrbl
@@ -1,28 +1,97 @@
 #lang scribble/manual
-@(require "../utils.rkt")
 
-@title[#:tag "lab01"]{1/10: Playing with objects}
+@(require scribble/eval
+          racket/sandbox
+          "../unnumbered.rkt"
+          "../utils.rkt"
+          (for-label class0
+                     2htdp/image
+                     2htdp/universe))
+
+@(define the-eval
+  (let ([the-eval (make-base-eval)])
+    (the-eval '(require lang/htdp-intermediate-lambda))
+    (the-eval '(require class0))
+    (the-eval '(require 2htdp/image))
+    (call-in-sandbox-context
+     the-eval
+     (lambda () ((dynamic-require 'htdp/bsl/runtime 'configure)
+                 (dynamic-require 'htdp/isl/lang/reader 'options))))
+    the-eval))
+
+@title[#:tag "lab01"]{1/10: Introduction to objects}
 
 @internal{
-Setup:
+
+@bold{TODO:}
 @itemlist[
-  @item{partner assignments}
-  @item{svn basics}
-  @item{svn exercise assn0}
-  @item{install teachpacks}
+  @item{#lang or ASL?}
+  @item{style for calling methods on @racket[this]?}
+  @item{@racket[big-bang] or World objects?}
+  @item{named args -> positional args}
 ]
 
-Objects:
-@itemlist[
-  @item{some simple world program in OO: structure + functions}
-    @item{no private}
-    @item{no inheritance}
-    @item{just fields and methods}
-  @item{define-class}
-  @item{define/public}
-  @item{fields}
-  @item{new}
-  @item{named arguments for constructors (only)}
-  @item{send}
+@section{Partner assignments}
+
+@section{Getting set up}
+
+@subsection{svn basics, submit assn0}
+
+@subsection{Install teachpacks}
+
+@section{Playing with objects}
+
+Here is a simple class in Racket:
+
+@#reader scribble/comment-reader
+(racketblock+eval #:eval the-eval
+  ; A Ball is a (new ball% Number Number Number Color)
+  (define-class ball%
+    (fields x y radius color)
+
+    ; Ball -> Boolean
+    ; Do the Balls have the same color?
+    (define/public (same-color? b)
+      (equal? (field color) (send b color)))
+
+    ; -> Image
+    ; The image representing the Ball
+    (define/public (draw)
+      (circle (field radius) "solid" (field color))))
+)
+
+Recall that we can create and use @racket[Ball] objects as follows:
+
+@interaction[#:eval the-eval
+  (define b (new ball% 50 25 10 "red"))
+  b
+  (send b x)
+  (send b radius)
+  (send b same-color? (new ball% 10 20 3 "red"))
 ]
+
+@exercise{
+  Write the method @racket[diameter] for the class @racket[ball%] that
+  calculates the Ball's diameter.
+}
+
+@exercise{
+  Write the method @racket[distance-to] for the class @racket[ball%] that
+  takes a Posn and returns the distance from the center of the Ball to the Posn.
+}
+
+@exercise{
+  Write the method @racket[overlaps?] for the class @racket[ball%] that takes
+  another Ball and determines whether the two Balls overlap.
+}
+
+@exercise{
+  Write a class @racket[block%] to represent rectangular blocks on the screen.
+  Include enough fields to record its position, size, and color. Construct
+  some examples of Blocks.
+
+  Write a @racket[draw] method for @racket[block%] that returns an image
+  representing the Block.
+}
+
 }
