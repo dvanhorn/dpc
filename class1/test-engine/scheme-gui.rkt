2a30364853faeb18a29bd0c25cef9c12179cc1c7
samth <samth@racket-lang.org>
2011-02-03 00:25:16 +0000

    add crappy test engine
    git-svn-id: http://svn.lambda-calcul.us/oo/trunk@262 5597a609-ba74-427a-a62e-28e6ec40ac72

---
+++ class1/test-engine/scheme-gui.rkt
@@ -0,0 +1,3 @@
+#lang racket/base
+(require "racket-gui.rkt")
+(provide (all-from-out "racket-gui.rkt"))
---
+++ class1/test-engine/scheme-tests.rkt
@@ -0,0 +1,4 @@
+#lang racket/base
+(require "racket-tests.rkt")
+(provide (all-from-out "racket-tests.rkt"))
+
