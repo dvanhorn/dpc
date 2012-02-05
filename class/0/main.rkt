3b82640a3dc7603d768b1b8e3de81749472bb543
Sam Tobin-Hochstadt <samth@racket-lang.org>
2012-02-05 09:39:52 -0500

    Refactor class/{0,1,2} to share code.
    Creates private directory which holds common class implementation code.
    Remove define-class-helper files.
    Remove define-class files.
    
    Progress toward bug #17.

--- class/0/main.rkt
+++ class/0/main.rkt
@@ -1,43 +1,90 @@
 #lang racket/base
-(require "define-class.rkt"
-         (except-in lang/htdp-intermediate-lambda define require #%module-begin
+(require (except-in lang/htdp-intermediate-lambda define require #%module-begin
                     define-struct image? 
                     check-expect check-within check-error check-range check-member-of)
-         "../test-engine/racket-tests.rkt")
+         "../test-engine/racket-tests.rkt"
+         "../private/utils.rkt"
+         (only-in racket/class send this define/private define/public)
+         (prefix-in isl+: 
+                    (combine-in
+                     (only-in lang/htdp-intermediate-lambda define)
+                     (only-in "../test-engine/racket-tests.rkt"
+                              check-expect check-within 
+                              check-error check-member-of
+                              check-range)))
+         racket/splicing          
+         (for-syntax syntax/parse racket/list
+                     racket/syntax racket/base
+                     "../private/utils-stxtime.rkt")
+         (prefix-in r: racket))
 
-(require (prefix-in isl+: lang/htdp-intermediate-lambda))
-(require (prefix-in r: racket))
-
-(provide (all-from-out "define-class.rkt")
-         (all-from-out lang/htdp-intermediate-lambda)
-         define test require provide
+(provide (all-from-out lang/htdp-intermediate-lambda)
+         define require provide
          all-defined-out only-in all-from-out except-in
          (except-out (all-from-out "../test-engine/racket-tests.rkt")
                      test))
 
-(require (for-syntax syntax/parse racket/base))
+(provide new send field fields this
+         define-class)
+;; REMOVE -- see bug 16
+(provide define/public define/private)
 
-(define-syntax (#%module-begin stx)
-  (syntax-parse stx 
-    [(_ forms ...)
-     #'(r:#%module-begin (reset-tests) forms ... (test) (reset-tests))]))
+(provide (rename-out [-module-begin #%module-begin]
+                     [-define-struct define-struct]))
 
-(provide #%module-begin)
-
-(provide define-struct)
-(require (for-syntax syntax/struct))
-(define-syntax (define-struct stx)
+(define-syntax (define-class stx)    
   (syntax-parse stx
-    [(_ name:id (field:id ...))
-     (with-syntax ([(_ _ _ acc ...) 
-                    (build-struct-names #'name (syntax->list #'(field ...)) #f #t)])
-       #'(r:define-struct name (field ...) #:transparent 
-			  #:property prop:custom-print-quotable 'never
-                          #:property prop:custom-write
-                          (r:λ (s p w?) 
-                            (fprintf p "(make-~a" 'name)
-                            (fprintf p " ~v" (acc s))
-                            ...
-                            (fprintf p ")"))))]))
+    [(define-class class%
+       ;; workaround for PR 12537
+       (~describe #:opaque "a field declaration" f:fields-spec) ~!
+       (~or (~var <definition> (member-def (syntax->list #'(f.fld ...))))
+            ce:test) ...)
+     #:fail-when (check-duplicate-identifier (syntax->list #'(f.fld ...)))
+     "duplicate field name"
+     #:fail-when (check-duplicate-identifier (syntax->list #'(<definition>.f ...)))
+     "duplicate method name"
+     #:with -class% (datum->syntax #f (syntax-e #'class%))
+     (with-syntax ([field (datum->syntax stx 'field)]
+                   [(the-fld ...) (generate-temporaries #'(f.fld ...))])
+       (quasisyntax
+        (begin          
+          ce ...
+          (define-syntax class% (class-name #'-class% 
+                                            (list (list #'f.fld #'the-fld) ...)
+                                            (list #'<definition>.f ...)))
+          (r:define -class%
+            (r:class/derived #,stx (class% r:object% 
+                                           (r:writable<%>
+                                            (r:interface* () ([prop:custom-print-quotable 'never])))
+                                           #f)
+              (r:inspect #f)
+              
+              (r:init-field [(the-fld f.fld)] ...)
+              (r:super-new)
+              (r:define/public (f.fld) the-fld)
+              ...
+              (splicing-let-syntax
+               ([field (λ (stx)
+                         (syntax-parse stx
+                           [(_ arg) 
+                            (let ([r (assf (λ (id) (eq? id (syntax-e #'arg)))
+                                           (list (list 'f.fld #'the-fld) ...))])
+                              (if r
+                                  (second r)
+                                  (raise-syntax-error #f 
+                                                      "no field by that name" 
+                                                      stx 
+                                                      #'arg)))]))])
+               (void)
+               (r:define/public (custom-write p) 
+                (fprintf p "(new ~a" 'class%)
+                (for ([i (list #,@(syntax->list #'(the-fld ...)))])
+                  (fprintf p " ~v" i))
+                (fprintf p ")"))
+               (r:define/public (custom-display p) (custom-write p))
+	       
+               <definition>.def
+               ...))))))]))
+
                   
     
--- class/1/main.rkt
+++ class/1/main.rkt
@@ -1,38 +1,118 @@
 #lang racket/base
-(require "define-class.rkt"
-         (except-in lang/htdp-intermediate-lambda define require #%module-begin
-                    define-struct image? quote #%app 
+(require (except-in lang/htdp-intermediate-lambda define require #%module-begin
+                    define-struct image? #%app
                     check-expect check-within check-error check-range check-member-of)
-         "../test-engine/racket-tests.rkt")
+         "../test-engine/racket-tests.rkt"
+         "../private/utils.rkt"
+         (only-in racket/class send this define/private define/public)
+         (prefix-in isl+: 
+                    (combine-in
+                     (only-in lang/htdp-intermediate-lambda define)
+                     (only-in "../test-engine/racket-tests.rkt"
+                              check-expect check-within 
+                              check-error check-member-of
+                              check-range)))
+         racket/splicing          
+         (for-syntax syntax/parse racket/list
+                     racket/syntax racket/base
+                     "../private/utils-stxtime.rkt")
+         (prefix-in r: racket))
 
-
-(require (only-in "../0/main.rkt" define-struct #%module-begin)
-         (for-syntax racket/base syntax/parse))
-(require (prefix-in isl+: lang/htdp-intermediate-lambda))
-(require (prefix-in r: racket))
-
-(provide (all-from-out "define-class.rkt")
-         (all-from-out lang/htdp-intermediate-lambda)
-	 quote
-         #%module-begin |.| (rename-out [my-app #%app])
-         define test require provide define-struct
+(provide (all-from-out lang/htdp-intermediate-lambda)
+	 quote |.| define require provide
          all-defined-out only-in all-from-out except-in
          (except-out (all-from-out "../test-engine/racket-tests.rkt")
                      test))
 
-(define-syntax (my-app stx)
-  (syntax-parse stx #:literals (|.|)
-    [(_ rcvr:expr (~and dot |.|) meth:id (~and (~not |.|) args:expr) ...)
-     (syntax-property #'(send rcvr meth args ...)
-                      'disappeared-use #'dot)]
-    [(_ rcvr:expr (~and dot |.|) meth:id (~and (~not |.|) args:expr) ... rest:expr ...)
-     (syntax-property #'(my-app (send rcvr meth args ...) rest ...)
-                      'disappeared-use #'dot)]
-    [(_ e ...)
-     #'(#%app e ...)]))
-      
-(define-syntax (|.| stx)
-  (raise-syntax-error #f "not legal outside of method send syntax" stx))
+(provide (rename-out [-module-begin #%module-begin]
+                     [-define-struct define-struct]
+                     [my-app #%app]))
+(provide new send field fields this
+         define-class define-interface super implements)
+
+(define-syntax (define-class stx)  
+  (syntax-parse stx #:literals (super implements fields)
+    [(define-class class%
+       (~optional (super super%:cls-name)
+                  #:defaults ([super%.real-name #'r:object%]
+                              [super%.fields null]
+                              [super%.methods null]))
+       ~!
+       (~optional (implements i%:ifc-name ...)
+                  #:defaults ([(i%.real-name 1) null]))
+       ~!       
+       ;; workaround for PR 12537
+       (~describe #:opaque "a field declaration" f:fields-spec)
+       ~!
+       (~or (~var <definition> (member-def (syntax->list #'(f.fld ...))))
+            ce:test)
+       ...)
+     #:fail-when (check-duplicate-identifier (syntax->list #'(f.fld ...)))
+     "duplicate field name"
+     #:fail-when (check-duplicate-identifier (syntax->list #'(<definition>.f ...)))
+     "duplicate method name"
+     #:with -class% (datum->syntax #f (syntax-e #'class%))
+     (with-syntax* ([field (datum->syntax stx 'field)]
+                    [(the-fld ...)
+                     (generate-temporaries (syntax (f.fld ...)))]
+                    [(super-methods ...) (attribute super%.methods)]
+                    [(meths ...) #'(super-methods ... <definition>.f ...)]
+                    [(inherit-fld ...)
+                     (map reverse (attribute super%.fields))]
+                    ;; the real names
+                    [(all-field-names ...)
+                     (append (map second (attribute super%.fields))
+                             (syntax->list #'(the-fld ...)))]
+                    ;; the external names
+                    [(all-flds ...)
+                     (append (map first (attribute super%.fields))
+                             (syntax->list #'(f.fld ...)))]
+                    [over (if (free-identifier=? #'super%.real-name #'r:object%)
+                              #'r:define/public ;; don't override if we're the top class
+                              #'r:define/override)])
+       (quasisyntax
+        (begin
+          ce ...
+          (define-syntax class% (class-name #'-class% 
+                                            (list (list #'all-flds #'all-field-names) ...)
+                                            (list #'meths ...)))
+          (r:define -class%
+            (r:class/derived #,stx (class% super%.real-name 
+                                           ((r:interface* () ([prop:custom-print-quotable 'never]))
+                                            r:writable<%> i%.real-name ...) #f)
+              (r:inspect #f)
+              
+              (r:inherit-field inherit-fld) ...
+              (r:init-field [(the-fld f.fld)] ...)
+              (r:inherit super-methods) ...
+              (r:super-new)
+              (r:define/public (f.fld) the-fld)
+              ...
+              (splicing-let-syntax
+               ([field 
+                  (λ (stx)
+                    (syntax-parse stx
+                      [(_ arg) 
+                       (let ([r (assf (λ (id) (eq? id (syntax-e #'arg)))
+                                      (list (list 'all-flds #'all-field-names) ...))])
+                         (if r
+                             (second r)
+                             (raise-syntax-error #f 
+                                                 "no field by that name" 
+                                                 stx 
+                                                 #'arg)))]))])
+               (void)
+               (over (custom-write p) 
+                (fprintf p "(new ~a" 'class%)
+                (for ([i (list #,@(append (syntax->list #'(the-fld ...))
+                                          (map second (attribute super%.fields))))])
+                  (fprintf p " ~v" i))
+                (fprintf p ")"))
+               (over (custom-display p) (custom-write p))
+	       
+               <definition>.def
+               ...))))))]))
+
 
 
                   
