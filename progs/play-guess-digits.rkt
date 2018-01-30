d3207d63dfb143b4b1125871b5215946acadb59c
David Van Horn <dvanhorn@cs.umd.edu>
2018-01-30 14:32:56 -0500

    Remove everything but class.

--- progs/play-guess-digits.rkt
+++
@@ -1,8 +0,0 @@
-#lang class/1
-(require class/universe)
-(require "guess-client-digits.rkt")
-(require "guess-server1.rkt")
-
-(launch-many-worlds
-  (big-bang (new no-guess%))
-  (universe (new thinking-of% (random 1000))))
